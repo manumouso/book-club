f9daf21e31e5b8e2ff7f5e768b78e338e07daaa4
manumouso <63980825+manumouso@users.noreply.github.com>
2022-11-13 11:11:06 -0300

    seed.ts
    seeder: probabilistically simulates book loans

--- back/prisma/seed.ts
+++ back/prisma/seed.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { users } from './users';
 import { authors } from './authors';
 import { genres } from './genres';
@@ -10,6 +11,8 @@ const prisma = new PrismaClient();
 
 async function main() {
   let userIds = [];
+  let authorIds = [];
+  let genreIds = [];
   for (let user of users) {
     let hash = await argon.hash(user.password);
     userIds.push(
@@ -24,8 +27,64 @@ async function main() {
       }),
     );
   }
-  const idsArr = userIds.map((userIds) => userIds.id);
-  console.log(idsArr);
+  for (let author of authors) {
+    authorIds.push(
+      await prisma.author.create({
+        data: author,
+        select: {
+          id: true,
+        },
+      }),
+    );
+  }
+  for (let genre of genres) {
+    genreIds.push(
+      await prisma.genre.create({
+        data: genre,
+        select: {
+          id: true,
+        },
+      }),
+    );
+  }
+
+  const userIdsArr = userIds.map((userIds) => userIds.id);
+  const authorIdsArr = authorIds.map((authorIds) => authorIds.id);
+  const genreIdsArr = genreIds.map((genreIds) => genreIds.id);
+  
+  for (let book of books) {
+    let holderPos = getHolderPos(userIdsArr.length);
+    let idHolder = null;
+    let withdrawn = null;
+    if (holderPos !== null) {
+      idHolder = userIdsArr[holderPos];
+      withdrawn = new Date();
+    }
+    await prisma.book.create({
+      data: {
+        isbn: book.isbn,
+        title: book.title,
+        year: book.year,
+        publisher: book.publisher,
+        ownerId: userIdsArr[getRandomPos(userIdsArr.length)],
+        holderId: idHolder,
+        withdrawnAt: withdrawn,
+        authorId: authorIdsArr[getRandomPos(authorIdsArr.length)],
+        genreId: genreIdsArr[getRandomPos(genreIdsArr.length)],
+      },
+    });
+  }
+}
+
+function getRandomPos(numberIds: number) {
+  return Math.floor(Math.random() * numberIds);
+}
+
+function getHolderPos(numberIds: number) {
+  if (Math.random() < 0.7) {
+    return Math.floor(Math.random() * numberIds);
+  }
+  return null;
 }
 
 main()
