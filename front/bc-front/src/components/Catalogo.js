54f04a91504877ab1fc6c539407e5b0bec533177
MaximoMag <maxmageiro@gmail.com>
2022-11-20 16:47:36 -0300

    cosas

--- front/bc-front/src/components/Catalogo.js
+++ front/bc-front/src/components/Catalogo.js
@@ -15,100 +15,128 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-
+import makeAnimated from "react-select/animated";
+import Select from 'react-select';
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const theme = createTheme();
 
 export default function Catalogo() {
+  
+  const opcionesBusqueda = [
+    {value: "isbn", label: "ISBN"},
+    {value: "year", label: "Año"},
+    {value: "publisher", label: "Editorial"},
+    {value: "Sinopsis", label: "synopsis"},
+    {value: "authorId", label: "ID de autor"},
+    {value: "firstName", label: "Primer nombre del autor"},
+    {value: "genreId", label: "ID del género"},
+    {value: "lastName", label: "Apellido del autor"},
+  ]
+  const [SelectedOption, setSelectedOptions] = React.useState();
+
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <AppBar position="relative">
-        <Toolbar style={{"display": "flex", "justifyContent": "space-around"}}>
-          <Button style={{"color": "white"}} href="/Catalogo">Catalogo</Button>
-          <Button style={{"color": "white"}} href="/MisLibros">Mis libros</Button>
-          <Button style={{"color": "white"}} href="/TusPrestamos">Prestamos</Button>
-        </Toolbar>
-      </AppBar>
-      <main>
-        {/* Hero unit */}
-        <Box
-          sx={{
-            bgcolor: 'background.paper',
-            pt: 8,
-            pb: 6,
-          }}
-        >
-          <Container maxWidth="sm">
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="text.primary"
-              gutterBottom
-            >
-              Catálogo
-            </Typography>
+    <div style={{"width": "100%"}}>
+       
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="relative">
+          <Toolbar style={{"display": "flex", "justifyContent": "space-around"}}>
+            <Button style={{"color": "white"}} href="/Catalogo">Catalogo</Button>
+            <Button style={{"color": "white"}} href="/MisLibros">Mis libros</Button>
+            <Button style={{"color": "white"}} href="/TusPrestamos">Prestamos</Button>
+          </Toolbar>
+        </AppBar>
+        <main>
+          {/* Hero unit */}
+          <Box
+            sx={{
+              bgcolor: 'background.paper',
+              pt: 8,
+              pb: 6,
+            }}
+          >
+            <Container maxWidth="sm">
+              <Typography
+                component="h1"
+                variant="h2"
+                align="center"
+                color="text.primary"
+                gutterBottom
+              >
+                Catálogo
+              </Typography>
+              <Select
+                placeholder={"Seleccione uno..."}
+                options={opcionesBusqueda}
+                onChange={(item) => {
+                  setSelectedOptions(item.value);
+                }}
+                isClearable={false}
+                isSearchable={true}
+                isDisabled={false}
+                isRtl={false}
+                closeMenuOnSelect={true}
+              />
+            </Container>
+          </Box>
+          <Container sx={{ py: 8 }} maxWidth="md">
+            {/* End hero unit */}
+            <Grid container spacing={4}>
+              {cards.map((card) => (
+                <Grid item key={card} xs={12} sm={6} md={4}>
+                  <Card
+                    sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                  >
+                      <Typography gutterBottom variant="h5" component="h2">
+                        nombre libro
+                      </Typography>
+                    <CardMedia
+                      component="img"
+                      sx={{
+                        // 16:9
+                        pt: '20px',
+                      }}
+                      image="https://source.unsplash.com/random"
+                      alt="random"
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                      {/* <Typography gutterBottom variant="h5" component="h2">
+                        Heading
+                      </Typography> */}
+                      <Typography>
+                        Datos libro
+                      </Typography>
+                      <Typography>
+                        fecha: 
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">Ver detalle</Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
           </Container>
-        </Box>
-        <Container sx={{ py: 8 }} maxWidth="md">
-          {/* End hero unit */}
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                    <Typography gutterBottom variant="h5" component="h2">
-                      nombre libro
-                    </Typography>
-                  <CardMedia
-                    component="img"
-                    sx={{
-                      // 16:9
-                      pt: '20px',
-                    }}
-                    image="https://source.unsplash.com/random"
-                    alt="random"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    {/* <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography> */}
-                    <Typography>
-                      Datos libro
-                    </Typography>
-                    <Typography>
-                      fecha: 
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">Ver detalle</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
-      </main>
-      {/* Footer */}
-      {/* <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          cosas de footer
-        </Typography>
-      </Box> */}
-      {/* End footer */}
-    </ThemeProvider>
+        </main>
+        {/* Footer */}
+        {/* <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+          <Typography variant="h6" align="center" gutterBottom>
+            Footer
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            align="center"
+            color="text.secondary"
+            component="p"
+          >
+            cosas de footer
+          </Typography>
+        </Box> */}
+        {/* End footer */}
+      </ThemeProvider>
+    </div>
   );
 }
