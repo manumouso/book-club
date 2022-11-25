1fe1e952be5105809fcd90d6ac9a35a63206bcfd
octex <56984557+octex@users.noreply.github.com>
2022-11-25 20:16:38 -0300

    Fixed borrow and return buttons

--- front/bc-front/src/components/Unauthorized.js
+++ front/bc-front/src/components/Unauthorized.js
@@ -2,10 +2,10 @@ import * as React from 'react';
 import DoNotDisturbIcon from '@mui/icons-material/DoNotDisturb';
 import { color } from '@mui/system';
 
-export default function Unauthorized() {
+const Unauthorized = () => {
     return (
-        <main>
-            <h1 style={{backgroundColor: "#9c27b0", color: "white"}}className='unAuthorized'><DoNotDisturbIcon fontSize='large' /><div > UNAUTHORIZED, Please <a href="/">Sign In</a>!</div> <DoNotDisturbIcon fontSize='large' /></h1>
-        </main>
-    );
+        <h1 style={{backgroundColor: "#9c27b0", color: "white"}}className='unAuthorized'><DoNotDisturbIcon fontSize='large' /><div > UNAUTHORIZED, Please <a href="/">Sign In</a>!</div> <DoNotDisturbIcon fontSize='large' /></h1>
+    )
 }
+
+export default Unauthorized
