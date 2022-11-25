9dd80d7ad35c65691d7facfd8d8a395ecc3cbc58
octex <56984557+octex@users.noreply.github.com>
2022-11-25 19:56:23 -0300

    Modified unauthorized view

--- front/bc-front/src/components/Unauthorized.js
+++ front/bc-front/src/components/Unauthorized.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import DoNotDisturbIcon from '@mui/icons-material/DoNotDisturb';
+import { color } from '@mui/system';
 
 export default function Unauthorized() {
     return (
         <main>
-            <h1 className='unAuthorized'><DoNotDisturbIcon fontSize='large' /><div > UNAUTHORIZED, Please <a href="/">Sign In</a>!</div> <DoNotDisturbIcon fontSize='large' /></h1>
+            <h1 style={{backgroundColor: "#9c27b0", color: "white"}}className='unAuthorized'><DoNotDisturbIcon fontSize='large' /><div > UNAUTHORIZED, Please <a href="/">Sign In</a>!</div> <DoNotDisturbIcon fontSize='large' /></h1>
         </main>
     );
 }
